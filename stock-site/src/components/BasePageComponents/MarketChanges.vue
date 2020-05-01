8c84ff50554049126baa7554f04581ff464e9c5d
pinna526 <xl330@126.com>
2020-05-01 16:14:28 +0800

    add directory

---
+++ stock-site/src/components/BasePageComponents/MarketChanges.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "MarketChanges"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ stock-site/src/components/BasePageComponents/MarketPrice.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        大盘行情
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MarketPrice"
+    }
+</script>
+
+<style scoped>
+
+</style>
