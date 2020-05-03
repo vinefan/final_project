50344fda6ec80b162d0fe622402864fbf6e56dc4
vinefan <17360078826@163.com>
2020-05-03 21:56:29 +0800

    test login

--- stock-site/src/main.js
+++ stock-site/src/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import axios from 'axios' //添加
+import axios from 'axios' 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import SlideVerify from 'vue-monoplasty-slide-verify';
 import VueRouter from 'vue-router'
 import router from './router'
+import VueAxios from 'vue-axios'
 
 
+Vue.use(VueAxios, axios)
 Vue.use(VueRouter)
 Vue.use(SlideVerify);
 Vue.prototype.$axios = axios;
